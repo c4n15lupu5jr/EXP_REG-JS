bad3017fca893cd2996b981eeaa854571dcf58f9
Renan Lobo <84355075+c4n15lupu5jr@users.noreply.github.com>
2025-02-24 15:11:11 -0300

    Update aula7.js

--- aula7.js
+++ aula7.js
@@ -3,7 +3,8 @@ const { cpfs2 } = require('./base');
 // ^ -> Começa com
 // $ -> Termina com
 // [^] -> Negação
+// m - multiline
 
 const cpf = '254.224.877-45';
-const cpfRegExp = /^(\d{3}\.)\d{3}\-\d{2}/g
+const cpfRegExp = /^(\d{3}\.)\d{3}\-\d{2}$/g
 console.log(cpf.match(cpfRegExp));
